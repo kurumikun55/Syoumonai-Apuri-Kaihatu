848460e0cd993b86fe38a61d56a78b6af65f0ff5
TaroYamada <taro_yamada@example.com>
2025-11-26 12:36:44 +0900

    ｈｈｈｈｈｈｈ

--- src/test1/Test1.java
+++ src/test1/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("aaaaaaa");
 		for(int i= 0;i<5;i++) {
-			System.out.println("こんにちは");
+			System.out.println("太一");
 		}
 	}
 
