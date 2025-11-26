09e8b0423ec80b86a48be9d991f8a73e5751b097
TaroYamada <taro_yamada@example.com>
2025-11-26 13:26:06 +0900

    windoメソッドついか

--- src/sample/Tougouban.java
+++ src/sample/Tougouban.java
@@ -3,11 +3,15 @@ package sample;
 
 
 import sakurai.Method;
+import watanabe.windo;
 
 public class Tougouban {
 
     public static void main(String[] args) {
     	Method start = new Method();
+    	windo windo=new windo();
+    	
 		start.start();
+		windo.addButton("クリックしてね");
     }
 }
--- src/watanabe/windo.java
+++ src/watanabe/windo.java
@@ -1,10 +1,23 @@
 package watanabe;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class windo {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-System.out.println("");
-	}
+	public static void addButton(JFrame frame) {
+        JButton button = new JButton("クリックしてね");
+        button.setFont(new Font("SansSerif", Font.BOLD, 50));
+        frame.add(button, BorderLayout.SOUTH);
 
+        // ボタンにアクションを追加
+        button.addActionListener(e -> {
+            JOptionPane.showMessageDialog(frame, "ボタンが押されました！");
+        });
+    }
 }
+
