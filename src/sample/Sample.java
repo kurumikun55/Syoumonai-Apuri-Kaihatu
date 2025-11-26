3fd1abda05e5c4f116a64ff7645593c3d13a40ae
nokog <nokog@10.105.96.172>
2025-11-26 11:21:23 +0900

    commit test

---
+++ src/sample/Sample.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        // コンストラクタの引数でアプリタイトルを設定
+        JFrame frame = new JFrame("デモアプリ");
+        // デスクトップに表示する画面のサイズ
+        frame.setSize(1920,1080);
+        
+        // このウィンドウをデスクトップ上に表示する
+        frame.setVisible(true);
+        
+        // 画面上に表示するメッセージ
+        JLabel label = new JLabel("Hello World");
+        label.setFont(new Font("Serif", Font.PLAIN, 250));
+        // メッセージを画面上部に表示させる
+        label.setVerticalAlignment(JLabel.TOP);
+        frame.add(label);
+        
+        // 画面上下左右中央にウィンドウを表示させる
+        frame.setLocationRelativeTo(null);
+        
+        // 「X」ボタンで画面を閉じたらアプリケーションを終了させる
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
