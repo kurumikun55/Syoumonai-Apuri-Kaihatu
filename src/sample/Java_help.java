dd05cbb003a13dd5fb092e636c775a71d939ec50
nokog <nokog@10.105.96.108>
2025-12-03 12:46:32 +0900

    java_help_app_methodにいたずらプログラムをメソッド化 java_helpに実行用のプログラムを作成

---
+++ src/sample/Java_help.java
@@ -0,0 +1,15 @@
+package sample;
+
+import sakurai.Java_help_app_method;
+
+public class Java_help {
+
+	public static void main(String[] args) {
+		//いたずらプログラムクラスのオブジェクトを生成
+		Java_help_app_method app = new Java_help_app_method();
+		//メソッド呼び出し
+		app.JavaHelpApp();
+
+	}
+
+}
